39c4f3e71160da546024640dcb35594b8b37f7dc
Florian Latapie <florian.latapie@etu.unice.fr>
2022-04-28 01:24:52 +0200

    Create ubuntu_gnome_extensions.sh

---
+++ linux/ubuntu/autre/ubuntu_gnome_extensions.sh
@@ -0,0 +1,6 @@
+# gome shell extensions
+echo -e "Les extensions sont disponibles sur le site : https://extensions.gnome.org"
+sudo apt install gnome-shell-extensions
+
+echo -e "\nOuverture dans chrome des extensions\n"
+google-chrome https://extensions.gnome.org/extension/2236/night-theme-switcher/
