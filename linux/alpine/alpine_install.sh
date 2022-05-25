bbff52f6e5202184627948864282c2a1b56d86c7
Florian Latapie <florian.latapie@etu.unice.fr>
2022-05-25 02:19:33 +0200

    ajout d'informations dans les echo

--- linux/alpine/alpine_install.sh
+++ linux/alpine/alpine_install.sh
@@ -5,16 +5,25 @@ apk upgrade
 ##    utile    ##
 
 # nano : editeur de texte
+echo -e "\nInstall nano\n"
 apk add nano
 
 # explorateur de fichiers 
+echo -e "\nInstall ranger\n"
 apk add ranger
 
 # explorateur de fichiers 
+echo -e "\nInstall mc\n"
 apk add mc
 
 # affiche le nom de la distro 
+echo -e "\nInstall neofetch\n"
 apk add neofetch
 
 # navigateur web en ligne de commande
+echo -e "\nInstall links\n"
 apk add links 
+
+# GitHub CLI 
+echo -e "\nInstall gh\n"
+apk add github-cli
