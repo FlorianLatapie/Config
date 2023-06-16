6bff5838e328168e784e9d4662c693b1983b2f30
Florian Latapie <70631622+FlorianLatapie@users.noreply.github.com>
2023-06-16 16:59:35 +0700

    Update bashrc_cours.sh

--- linux/ubuntu/bashrc_cours.sh
+++ linux/ubuntu/bashrc_cours.sh
@@ -1,5 +1,7 @@
-## Cours  
-export PATH=$PATH:.
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.:/usr/lib/jvm/java-17-openjdk-amd64/lib/server/
-export LD_LIBRARY_PATH
+## Cours 
+
+### SI3
+#export PATH=$PATH:.
+#LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.:/usr/lib/jvm/java-17-openjdk-amd64/lib/server/
+#export LD_LIBRARY_PATH
 
