92118849e088793994c4d9fe068dfba8d25967db
Florian Latapie <70631622+FlorianLatapie@users.noreply.github.com>
2023-06-16 17:53:45 +0700

    Update wsl_run.sh

--- linux/ubuntu/wsl_run.sh
+++ linux/ubuntu/wsl_run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo -e "Exécution de wsl_run.sh\n"
-./autre/wsl_run.sh
-
 echo -e "Exécution de run.sh\n"
 ./run.sh
+
+echo -e "Exécution de wsl_run.sh\n"
+./autre/wsl_run.sh
