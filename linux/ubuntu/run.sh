08aae7de26b9c0e97355a39e61872e8021ceba50
Florian Latapie <florian.latapie@etu.unice.fr>
2023-08-03 17:01:38 +0700

    update of config for ubuntu and wsl

--- linux/ubuntu/run.sh
+++ linux/ubuntu/run.sh
@@ -4,11 +4,9 @@ echo -e "Mise à jour et installation\n"
 ./ubuntu_install.sh
 
 echo -e "Ajout de la config dans le .bashrc\n"
-cat bashrc_cours.sh>> ~/.bashrc
+cat bashrc_cours.sh >> ~/.bashrc
 cat bashrc.sh >> ~/.bashrc
 
-echo -e "Terminé\n"
+source ~/.bashrc
 
-# On relance le bash pour que les commandes soient prises en compte
-# lolcat notamment 
-bash
+echo -e "Terminé\n"
