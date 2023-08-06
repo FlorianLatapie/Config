e73c04fefc19a9144c58aa34233187484f4276fb
Florian Latapie <florian.latapie@etu.unice.fr>
2023-08-07 00:51:47 +0700

    Update run.sh

--- linux/alpine/run.sh
+++ linux/alpine/run.sh
@@ -7,9 +7,9 @@ echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 
 echo -e "\n\nAttention, il faut utiliser la commande passwd pour définir un mot de passe pour root (utile pour le serveur SSh\n\n"
 
-echo -e "Bienvenue dans alpine !\n\nVous pouvez installer des paquets avec : apk add <nom_du_paquet>\n\nVous pouvez changer ce message en éditant le fichier /etc/motd.\n\nLancer le serveur SSH avec la commande : /usr/sbin/sshd\n\n"
+echo -e "Bienvenue dans alpine !\n\nVous pouvez installer des paquets avec : apk add <nom_du_paquet>\n\nVous pouvez changer ce message en éditant le fichier /etc/motd.\n\nLancer le serveur SSH avec la commande : /usr/sbin/sshd\n\n" >> /etc/motd
 
-cat bashrc.sh > ~/.bashrc
+cat bashrc.sh >> ~/.bashrc
 source ~/.bashrc
 
 echo -e "Terminé\n"
