296ada4043f477d88cedb62c6d881b4bf712d5b8
Florian Latapie <florian.latapie@etu.unice.fr>
2023-08-06 21:21:01 +0700

    cleaner alpine install

--- linux/alpine/alpine_install.sh
+++ linux/alpine/alpine_install.sh
@@ -1,22 +1,16 @@
 apk update
 apk upgrade
 
-# nano : éditeur de texte
-echo -e "\nInstall nano\n"
-apk add nano
+packages=(
+    nano
+    ranger
+    mc
+    neofetch
+    links
+    curl
+)
 
-# explorateur de fichiers 
-echo -e "\nInstall ranger\n"
-apk add ranger
-
-# explorateur de fichiers 
-echo -e "\nInstall mc\n"
-apk add mc
-
-# affiche le nom de la distro 
-echo -e "\nInstall neofetch\n"
-apk add neofetch
-
-# navigateur web en ligne de commande
-echo -e "\nInstall links\n"
-apk add links 
+for package in "${packages[@]}"; do
+    echo -e "\nInstall $package\n"
+    apk add "$package"
+done
