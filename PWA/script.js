846b044f138a1928a230b7685d228db839041162
Florian Latapie <florian.latapie@etu.unice.fr>
2023-12-19 14:23:26 +0100

    add css

--- PWA/script.js
+++ PWA/script.js
@@ -2,29 +2,39 @@
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('./sw.js')
-    .then(() => { console.log('Service Worker Registered'); })
-    .catch((e) => { console.log('Service Worker Registration Failed'); console.error(e); });
+  .register('./sw.js')
+  .then(() => { console.log('Service Worker Registered'); })
+  .catch((e) => { console.log('Service Worker Registration Failed'); console.error(e); });
 }
 
 
 let deferredPrompt;
-const installBtn = document.querySelector('#installBtn');
+const installBtn = document.getElementById('installBtn');
+const iosText = document.getElementById("iosText");
+
 console.log(localStorage.getItem('a2hs'));
 
 if (localStorage.getItem('a2hs') === 'accepted') {
   installBtn.style.display = 'none';
 }
 
+
 window.addEventListener('beforeinstallprompt', (e) => {
   e.preventDefault();
   deferredPrompt = e;
-
-  installBtn.style.display = 'block';
-
+  
+  if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) {
+    installBtn.style.display = "none";
+    iosText.style.display = "block";
+    console.log("ios");
+  } else {
+    installBtn.style.display = "block";
+    iosText.style.display = "none";
+    console.log("not ios");
+  }
   installBtn.addEventListener('click', () => {
     installBtn.style.display = 'none';
-
+    
     deferredPrompt.prompt();
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === 'accepted') {
@@ -41,25 +51,25 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
 
 if (window.matchMedia('(display-mode: standalone)').matches) {
-    document.getElementById("installBtn").style.display = "none";
-    const response = confirm("Vous avez déjà installé Config !\nVous allez être redirigé vers la page d'accueil.");
-    if (response === true){
-        gotoHome();
-    }
+  document.getElementById("installBtn").style.display = "none";
+  const response = confirm("Vous avez déjà installé Config !\nVous allez être redirigé vers la page d'accueil.");
+  if (response === true){
+    gotoHome();
+  }
 }
 
 const goHomeBtn = document.getElementById("goHomeBtn");
 const goRepoBtn = document.getElementById("goRepoBtn");
 
 goHomeBtn.addEventListener('click', () => {
-    gotoHome();
+  gotoHome();
 });
 
 goRepoBtn.addEventListener('click', () => {
-    window.location.href = "https://github.com/florianlatapie/config";
+  window.location.href = "https://github.com/florianlatapie/config";
 });
 
 
 function gotoHome() {
-    window.location.href = "../";
+  window.location.href = "../";
 }
